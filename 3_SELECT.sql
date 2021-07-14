ebbd56c784f7e763cceeaad93893f8b7700b80c7
ankitverma5859 <47997907+ankitverma5859@users.noreply.github.com>
2021-07-14 19:31:36 +0530

    Added example for Aliases

--- 3_SELECT.sql
+++ 3_SELECT.sql
@@ -1,5 +1,5 @@
 /*
-  __How to select all data from a Table?
+  __How to select all data/columns from a Table?
   Syntax:   SELECT * FROM <TableName>
   Example:  
 */
@@ -8,4 +8,42 @@
             SQL keywords like SELECT, FROM, etc are also case-insensitive.
             In the below query, FROM clause is executed first i.e before the SELECT clause.
         */
-        SELECT * FROM movies;
+        
+        SELECT * 
+        FROM movies;
+
+/*
+  __How to select particular columns from a Table?
+  Syntax:   SELECT <ColumnName>, <ColumnName> ... FROM <TableName>
+  Example:  
+*/
+        SELECT movie_id, movie_name 
+        FROM movies;
+        
+/*
+  __How to use Aliases?
+  Aliases are used to give better readability to the column names.
+  Syntax:   SELECT 
+              <ColumnName> AS <AliasName>, 
+              <ColumnName> AS <AliasName>, 
+              ... 
+            FROM <TableName>
+  Example:  
+*/     
+        SELECT 
+          first_name AS "FirstName"
+        FROM actors;
+        
+        SELECT 
+          movie_name AS "Movie Name",
+          movie_lang AS "Language"
+        FROM movies;
+        
+        /* AS is a optional keyword for the query, but its recommended to use AS for readability. */
+        SELECT 
+          movie_name "Movie Name",
+          movie_lang "Language"
+        FROM movies;
+
+        
+        
