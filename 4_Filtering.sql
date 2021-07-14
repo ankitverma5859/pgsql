2e4d52a86937a41c9f48f58683e387758d9032d1
ankitverma5859 <47997907+ankitverma5859@users.noreply.github.com>
2021-07-14 22:43:09 +0530

    Update 4_Filtering.sql

--- 4_Filtering.sql
+++ 4_Filtering.sql
@@ -32,46 +32,46 @@ ARITHMETIC
 	    WHERE <ColumnName> = 'value'    //value should always be in single quotes
   Example:  
 */
-	 SELECT * 
-	 FROM movies
-	 WHERE movie_lang = 'English'; 
+		 SELECT * 
+		 FROM movies
+		 WHERE movie_lang = 'English'; 
 
-	 /* Using AND operator */
-	 SELECT * 
-	 FROM movies
-	 WHERE 
-		movie_lang = 'English' AND age_certificate = '18';
+		 /* Using AND operator */
+		 SELECT * 
+		 FROM movies
+		 WHERE 
+			movie_lang = 'English' AND age_certificate = '18';
 
-	 /* Using OR operator */
-	 SELECT * 
-	 FROM movies
-	 WHERE 
-		movie_lang = 'English' OR age_certificate = '18';
+		 /* Using OR operator */
+		 SELECT * 
+		 FROM movies
+		 WHERE 
+			movie_lang = 'English' OR age_certificate = '18';
 
-	 SELECT * 
-	 FROM movies
-	 WHERE 
-		movie_lang = 'English' OR movie_lang = 'Chinese'
-	ORDER BY movie_lang;
+		 SELECT * 
+		 FROM movies
+		 WHERE 
+			movie_lang = 'English' OR movie_lang = 'Chinese'
+		ORDER BY movie_lang;
 
-	/* 
-		Combining AND and OR operators 
-		Order of Operators and parenthesis matters.
-	*/
-	SELECT * 
-	FROM movies
-	WHERE
-		(movie_lang = 'English' OR movie_lang = 'Chinese') AND age_certificate = '12'
-	ORDER BY movie_lang;
+		/* 
+			Combining AND and OR operators 
+			Order of Operators and parenthesis matters.
+		*/
+		SELECT * 
+		FROM movies
+		WHERE
+			(movie_lang = 'English' OR movie_lang = 'Chinese') AND age_certificate = '12'
+		ORDER BY movie_lang;
 
-	/*
-		Can we use WHERE before FROM?
-		NO. It will throw error.
+		/*
+			Can we use WHERE before FROM?
+			NO. It will throw error.
 
-		Can we use WHERE after ORDER BY?
-		NO. It will throw error.
+			Can we use WHERE after ORDER BY?
+			NO. It will throw error.
 
-		WHERE can only be used before ORDER BY and after FROM.
-	*/
+			WHERE can only be used before ORDER BY and after FROM.
+		*/
 		 
 		 
