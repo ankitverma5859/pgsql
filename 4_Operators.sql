9beab2b683fdc94be018461e45f554e630aaf2a4
ankitverma5859 <47997907+ankitverma5859@users.noreply.github.com>
2021-07-14 22:23:08 +0530

    Create 4_Operators.sql

---
+++ 4_Operators.sql
@@ -0,0 +1,25 @@
+/* 
+Operators
+			
+Comparison Operators:
+	1. Equal To 					=
+	2. Greater Than					>
+	3. Less Than					<
+	4. Greater Than or Equal To		>=
+	5. Less Than or Equal To		<=
+	6. Not Equal To					!=
+	
+Logical Operators
+	1. AND
+	2. OR
+	3. LIKE
+	4. IN
+	5. BETWEEN
+	
+ARITHMETIC 
+	1. +
+	2. _ 
+	3. /
+	4. *
+	5. % (modulas) 
+*/
