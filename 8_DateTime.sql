6186bfb1733dded3d0734692d6ea2b25f5078825
ankitverma5859 <47997907+ankitverma5859@users.noreply.github.com>
2021-07-19 17:26:46 +0530

    Create 8_DateTime.sql

---
+++ 8_DateTime.sql
@@ -0,0 +1,10 @@
+/* 
+DateTime
+
+	Date					Stores only date
+	Time					Stores only time
+	Timestamp				Stores date and time
+	Timestamptz				Stores date, time, and timezone
+	
+	Inteval					Stores difference between two date/time data types.
+*/
